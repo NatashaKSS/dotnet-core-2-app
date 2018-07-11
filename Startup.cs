0d2d3f461688641694a679bf51d06fd8ab5e7840
NatashaKSS <NatashaKSS@users.noreply.github.com>
2018-07-11 22:31:14 +0800

    Set up Development env variable
    * in launchSettings too, but it doesn't seem to overwrite VisualStudio's own specified environment variables

--- Startup.cs
+++ Startup.cs
@@ -61,7 +61,7 @@ namespace dotnet_core_app
             app.Run(async (context) =>
             {
                 var greeting = greeter.GetMessageOfTheDay();
-                await context.Response.WriteAsync(greeting);
+                await context.Response.WriteAsync($"{greeting} : {env.EnvironmentName}");
             });
         }
     }
