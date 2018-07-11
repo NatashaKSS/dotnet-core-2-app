439745fe114944ad6fcde48aded1d30d58ca359e
NatashaKSS <NatashaKSS@users.noreply.github.com>
2018-07-11 23:08:44 +0800

    Add homecontroller class
    * base class that handles requests directly from the index '/' route

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿namespace dotnetcoreapp.Controllers
+{
+    public class HomeController // requests come from root of application
+    {
+        public string Index()
+        {
+            return "Hello from the HomeController!";
+        }
+    }
+}
